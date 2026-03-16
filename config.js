4e6d679a57a12824540404495765210f8c1bfd59
Marvins-spec <littlebunny1900@gmail.com>
2026-03-16 11:29:47 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,36 @@
+const CONFIG = {
+
+API_URL:"https://script.google.com/macros/s/AKfycbyHGKbRD_qm0U1nVZhI4scd7g1dDVhFlThlMGpWI0Uwqim8TfF-09Dq8Bq3F0NS9-z1/exec",
+
+MENU:{
+
+pizza:{
+"Pizza Margherita":390,
+"Pizza Prosciutto":420,
+"Pizza Cheese":390,
+"Pizza Veggie Delight":390
+},
+
+dessert:{
+"Tiramisu":120,
+"Panna Cotta":120
+},
+
+drink:{
+"Cola":50,
+"Italian Soda":90,
+"Sparkling Water":50
+},
+
+side:{
+"Wings & Fries Combo":320
+},
+
+set:{
+"Small Set":890,
+"Large Set":1190
+}
+
+}
+
+}
